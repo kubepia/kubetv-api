031b93f490c1eb88c8cac54f8f03d0b4b91ec9f8
myguddy <seungil.han@gmail.com>
2020-02-16 21:47:40 +0900

    init

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+let express = require("express");
+let router = express.Router();
+let Client = require("node-rest-client").Client;
+
+let client = new Client();
+client.registerMethod("getContent", "http://localhost:8080/", "GET");
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+    res.render("index", { title: "Express" });
+});
+
+router.post("/login", (req, res, next) => {
+    if ("Passw0rd" === req.body.password) {
+        console.log(`user: ${req.body.user}`);
+        res.json({
+            status: "ok"
+        });
+    } else {
+        console.log("password should be 'Passw0rd'");
+        res.json({
+            status: "error",
+            message: "password should be 'Passw0rd'"
+        });
+    }
+});
+
+router.get("/logout", (req, res, next) => {
+    console.log("log out");
+    res.json({
+        status: "ok"
+    });
+});
+
+router.get("/account", (req, res, next) => {});
+router.put("/account", (req, res, next) => {});
+
+router.get("/content", (req, res, next) => {
+    let args = {};
+    client.methods.getContent(args, (data, response) => {
+      if(500 == response.statusCode){
+        res.statusCode(500).json(data)
+      }else{
+        res.json(data);
+      }
+      
+    });
+});
+router.get("/content/:id", (req, res, next) => {});
+router.get("/contents/:page", (req, res, next) => {});
+module.exports = router;
